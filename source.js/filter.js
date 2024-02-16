f32b713916fa9dc458c739adeaa63ee831707e31
FunnyRockfish <123645724+FunnyRockfish@users.noreply.github.com>
2024-02-16 10:17:25 +0300

    Update filter.js

--- source.js/filter.js
+++ source.js/filter.js
@@ -1,52 +1,20 @@
-const checkIndex = (index, indexMas) => indexMas.some(([start, end]) => index >= start && index <= end);
-
-const saveText = (text, allowedTags, tagIndexes) => {
-    const map = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#39;',
-    };
-    return text.replaceAll(/[&<>"'\s]/g, (match, index) =>
-        checkIndex(index, tagIndexes) ? match : map[match] || match
-    );
-};
-
-const findClosingTag = (text, tagName) => {
-    const openingTagRegex = new RegExp(`<${tagName}\\b[^>]*>`, 'g');
-    const closingTagRegex = new RegExp(`</${tagName}>`, 'g');
-    const openingTags = [...text.matchAll(openingTagRegex)];
-    const closingTags = [...text.matchAll(closingTagRegex)];
-    const tagsIndex = [];
-
-    for (let i = 0; i < openingTags.length; i++) {
-        let openingTagIndex = openingTags[i].index;
-        let flag = false;
-
-        for (let j = 0; j < closingTags.length; j++) {
-            let closingTagIndex = closingTags[j].index;
-
-            if (closingTagIndex > openingTagIndex) {
-                let openingTagLength = openingTags[i][0].length;
-                tagsIndex.push(openingTagIndex, closingTagIndex + openingTagLength);
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            tagsIndex.push(openingTagIndex, text.length);
-        }
-    }
-    return tagsIndex;
-};
 
 const filter = (input, allowedTags) => {
     const tagRegExp = /<\/?([a-zA-Z0-9]+)[^>]*>/g;
-    const matches = [...input.matchAll(tagRegExp)];
-    const tagsMas = matches
+    const tagsMas = [...input.matchAll(tagRegExp)]
         .map(match => match[0].substring(1, match[0].length - 1))
         .filter(tag => allowedTags.includes(tag))
-        .map(match => findClosingTag(input, match));
-    return saveText(input, allowedTags, tagsMas);
+        .map(tag => {
+            const openingTagRegex = new RegExp(`<${tag}\\b[^>]*>`, 'g');
+            const closingTagRegex = new RegExp(`</${tag}>`, 'g');
+            const openingTagMatch = openingTagRegex.exec(input);
+            const openingTagIndex = openingTagMatch ? openingTagMatch.index : 0;
+            const closingTagMatch = closingTagRegex.exec(input.slice(openingTagIndex));
+            const closingTagIndex = closingTagMatch ? openingTagIndex + closingTagMatch.index + closingTagMatch[0].length : input.length;
+            return [openingTagIndex, closingTagIndex];
+        });
+    const map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' };
+    return input.replaceAll(/[&<>"'\s]/g, (match, index) =>
+        tagsMas.some(([start, end]) => index >= start && index <= end) ? match : map[match] || match
+    );
 };
